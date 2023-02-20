18b496837e3c17f374d976a057abdbafd8a91467
donits09 <98079068+donits09@users.noreply.github.com>
2023-02-20 15:35:15 +0800

    updated_links

--- admin/final_approval/index.php
+++ admin/final_approval/index.php
@@ -3,6 +3,81 @@
 	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
 <?php endif;?>
+<style>
+	.main_menu{
+		float:left;
+		width:227px;
+		height:40px;
+		line-height:40px;
+		text-align:center;
+		color:black!important;
+	}
+	.navbar{
+		width:100%;
+		height:auto;
+	}
+	.main_menu:hover{
+		border-bottom: solid 2px blue;
+		background-color:#E8E8E8;
+	}
+	#container{
+		margin-right:auto;
+		margin-left:auto;
+		width:100%;
+		position:relative;
+		padding-left:50px;
+		padding-right:50px;
+		background-color:transparent;
+	}
+	#fa-link{
+		border-bottom: solid 2px blue;
+		background-color:#F5F5F5;
+	}
+	.dropdown:hover .dropdown-menu {
+		display: block;
+		margin-top:40px;
+		float:left;
+		width:227px;
+		height:130px;
+		line-height:30px;
+		text-align:center;
+		color:black!important;
+	}
+	.dropdown-menu li a{
+		color:black!important;
+		border:gainsboro 1px solid;
+		display: block;
+		height:40px;
+		line-height:40px;
+	}
+	.dropdown-menu li a:hover{
+		color:black!important;
+		border:gainsboro 1px solid;
+		display: block;
+		height:40px;
+		line-height:40px;
+		background-color:#E8E8E8;
+	}
+</style>
+
+<div class="card" id="container">
+    <div class="navbar-menu">
+		<div class="dropdown">
+			<a href="#" class="main_menu dropdown-toggle" id="ra-link" onclick="highlightLink('ralink')">RA List</a>
+				<ul class="dropdown-menu">
+					<li><a href="<?php echo base_url ?>admin/?page=sales">Pending</a></li>
+					<li><a href="<?php echo base_url ?>admin/?page=revision">Revision</a></li>
+					<li><a href="<?php echo base_url ?>admin/?page=ra">Approved</a></li>
+				</ul>
+		</div>
+		<a href="<?php echo base_url ?>admin/?page=reservation" class="main_menu" id="res-link" onclick="highlightLink('res-link')">Reservation List</a>
+		<a href="<?php echo base_url ?>admin/?page=credit_assestment" class="main_menu" id="ca-link" onclick="highlightLink('ca-link')">Credit Assessment List</a>
+		<a href="<?php echo base_url ?>admin/?page=final_approval" class="main_menu" id="fa-link" onclick="highlightLink('fa-link')">CFO Approval List</a>
+		<a href="<?php echo base_url ?>admin/?page=clients/property_list" class="main_menu" id="pl-link" onclick="highlightLink('pl-link')">Property Accounts</a>
+	</div>
+</div>
+
+
 <div class="card card-outline rounded-0 card-maroon">
 	<div class="card-header">
 		<h3 class="card-title">CFO Approval List</h3>
