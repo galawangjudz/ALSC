357d67411b45a74060e0ae280e5d5c2f78b80118
donits09 <98079068+donits09@users.noreply.github.com>
2023-03-13 14:19:18 +0800

    add_cost

--- admin/sales/create.php
+++ admin/sales/create.php
@@ -1228,7 +1228,7 @@ input{
 										</div>
 										<div class="col-md-4">
 											<div class="form-group">
-												<input type="text" class="form-control margin-bottom" id="others_subtotal" name="others_subtotal"  placeholder="0">
+												<input type="text" class="form-control margin-bottom" id="others_subtotal" name="others_subtotal"  placeholder="0" onkeyup="getAddCost()">
 											</div>
 										</div>
 									</div>
@@ -2004,7 +2004,7 @@ input{
 
 		var result = parseInt(others) + parseInt(service) + parseInt(ac_outlet) + parseInt(flr_elev)+ parseInt(conv_outlet) + parseInt(ac_grill1);
 
-		document.getElementById('addcost_total').value = result;
+		document.getElementById('add_cost_total').value = result;
 	}
 </script>
 
