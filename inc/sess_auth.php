3d86b5fcd65475411072d19cec7bb3f3ea43058f
galawangjudz <jaevoli@gmail.com>
2023-10-04 09:33:39 +0800

    add inc

--- inc/sess_auth.php
+++ inc/sess_auth.php
@@ -9,13 +9,12 @@ else
 $link .= "://"; 
 $link .= $_SERVER['HTTP_HOST']; 
 $link .= $_SERVER['REQUEST_URI'];
-
-
 if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
 	redirect('auth/login.php');
 }
 
- if(isset($_SESSION['userdata']) && strpos($link, 'login.php') && $_SESSION['userdata']['user_type'] == 'IT Admin'){
+echo  $_SESSION['userdata']['user_type'];
+if(isset($_SESSION['userdata']) && strpos($link, 'login.php') && $_SESSION['userdata']['user_type'] == 'IT Admin'){
 	redirect('admin/index.php');
 } 
 
