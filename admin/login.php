834765f92aa0830a1c78ce3f8aedde4bf390591e
galawangjudz <jaevoli@gmail.com>
2024-12-21 15:35:20 +0800

    add base_url

--- admin/login.php
+++ admin/login.php
@@ -54,11 +54,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?php echo base_url ?>dist/js/adminlte.min.js"></script>
 
 <script>
   $(document).ready(function(){
