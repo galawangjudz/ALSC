17d3625106c4cfa0aba270e43763516125fe1ff5
galawangjudz <jaevoli@gmail.com>
2024-12-21 15:40:29 +0800

    adjustment

--- auth/login.php
+++ auth/login.php
@@ -136,11 +136,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<!-- <script src="plugins/jquery/jquery.min.js"></script> -->
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<!-- <script src="dist/js/adminlte.min.js"></script> -->
 
 <script>
   $(document).ready(function(){
