37322a669554bcad7f81168ce3bcdbf2378efa8d
galawangjudz <jaevoli@gmail.com>
2025-01-06 10:30:52 +0800

    cm

--- initialize.php
+++ initialize.php
@@ -1,13 +1,13 @@
 <?php
-if(!defined('base_url')) define('base_url','http://localhost/ALSC/');
-//if(!defined('base_url')) define('base_url','http://wapps.asianland.ph/');
+//if(!defined('base_url')) define('base_url','http://localhost/ALSC/');
+if(!defined('base_url')) define('base_url','http://wapps.asianland.ph/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"alscdb");
-//if(!defined('DB_USERNAME')) define('DB_USERNAME',"u375134375_root");
-//if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Testtest11**");
-//if(!defined('DB_NAME')) define('DB_NAME',"u375134375_alscdb");
+//if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+//if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+//if(!defined('DB_NAME')) define('DB_NAME',"alscdb");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"u375134375_root");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Testtest11**");
+if(!defined('DB_NAME')) define('DB_NAME',"u375134375_alscdb");
 ?>
